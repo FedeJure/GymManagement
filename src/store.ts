3a1cc84f3cde547d5cbc10ed403dfc80e2b7fe22
Federico Jure <fedejure@gmail.com>
2021-09-04 23:48:32 -0300

    add navigation

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,9 @@
 import { combineReducers, createStore } from "redux";
+import { product, ProductState } from "./modules/product/product.reducer"
+import { navigation, NavigationState } from "./modules/navigation/navigation.reducer"
 
-export const store = createStore(combineReducers({}))
+export interface StoreState {
+    product: ProductState,
+    navigation: NavigationState
+}
+export const store = createStore(combineReducers({ product: product, navigation: navigation }))
