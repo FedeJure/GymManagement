9e661968dbac1166716a11d51d5dac81d34371ec
Federico Jure <fedejure@gmail.com>
2021-09-06 11:49:17 -0300

    add image upload without logi

--- src/components/createUserModal/CreateUserModal.tsx
+++ src/components/createUserModal/CreateUserModal.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react"
-import { Button, Modal, Form } from 'semantic-ui-react'
+import { useRef, useState } from "react"
+import { Button, Modal, Form, Grid, Divider, Segment, Icon, Header, Input } from 'semantic-ui-react'
 import { UserPayload } from "../../modules/users/users.reducer"
 
 const defaultDate = new Date(0)
 
 export const CreateUserModal = ({ onClose, onSubmit, initialData }:
     { onClose: any, onSubmit: any, initialData: UserPayload }) => {
+
+    const fileRef = useRef<HTMLInputElement>(null)
     const [formData, setFormData] = useState<UserPayload>(
         initialData ?
             initialData :
@@ -41,54 +43,77 @@ export const CreateUserModal = ({ onClose, onSubmit, initialData }:
             open
         >
             <Modal.Header>{initialData ? "Edición de persona" : "Creación de persona"}</Modal.Header>
-            <Modal.Content image>
-                <Form onSubmit={handleSubmit}>
-
-                    <Form.Field inline>
-                        <label>Nombre</label>
-                        <input placeholder=''
-                            value={formData.name}
-                            onChange={(v) => handleChange(v.currentTarget.value, "name")} />
-                    </Form.Field>
-                    <Form.Field inline>
-                        <label>Apellido</label>
-                        <input placeholder=''
-                            value={formData.lastname}
-                            onChange={(v) => handleChange(v.currentTarget.value, "lastname")} />
-                    </Form.Field>
-                    <Form.Field inline>
-                        <label>Fecha de nacimiento</label>
-                        <input placeholder=''
-                            type="date"
-                            value={formData.birthDate && formData.birthDate !== defaultDate ? formData.birthDate.toISOString().split("T")[0] : ""}
-                            onChange={(v) => handleChange(new Date(v.currentTarget.value), "birthDate")} />
-                    </Form.Field>
-                    <Form.Field inline>
-                        <label>Teléfono de contacto</label>
-                        <input placeholder=''
-                            value={formData.contactPhone}
-                            onChange={(v) => handleChange(v.currentTarget.value, "contactPhone")} />
-                    </Form.Field>
-                    <Form.Field inline>
-                        <label>Email de contacto</label>
-                        <input placeholder=''
-                            value={formData.contactEmail}
-                            onChange={(v) => handleChange(v.currentTarget.value, "contactEmail")} />
-                    </Form.Field>
-                    <Form.Field inline>
-                        <label>Domicilio</label>
-                        <input placeholder=''
-                            value={formData.address}
-                            onChange={(v) => handleChange(v.currentTarget.value, "address")} />
-                    </Form.Field>
-                    <Form.Field inline>
-                        <label>Comentario</label>
-                        <textarea placeholder=''
-                            value={formData.comment}
-                            onChange={(v) => handleChange(v.currentTarget.value, "comment")} />
-                    </Form.Field>
-                    <button type="submit" hidden={true} />
-                </Form>
+            <Modal.Content>
+                <Segment placeholder>
+                    <Grid columns={2} stackable>
+                        <Divider vertical />
+                        <Grid.Row>
+                            <Grid.Column verticalAlign='middle'>
+                                <Form onSubmit={handleSubmit}>
+                                    <Form.Field>
+                                        <label>Nombre</label>
+                                        <input placeholder=''
+                                            value={formData.name}
+                                            onChange={(v) => handleChange(v.currentTarget.value, "name")} />
+                                    </Form.Field>
+                                    <Form.Field >
+                                        <label>Apellido</label>
+                                        <input placeholder=''
+                                            value={formData.lastname}
+                                            onChange={(v) => handleChange(v.currentTarget.value, "lastname")} />
+                                    </Form.Field>
+                                    <Form.Field >
+                                        <label>Fecha de nacimiento</label>
+                                        <input placeholder=''
+                                            type="date"
+                                            value={formData.birthDate && formData.birthDate !== defaultDate ? formData.birthDate.toISOString().split("T")[0] : ""}
+                                            onChange={(v) => handleChange(new Date(v.currentTarget.value), "birthDate")} />
+                                    </Form.Field>
+                                    <Form.Field >
+                                        <label>Teléfono de contacto</label>
+                                        <input placeholder=''
+                                            value={formData.contactPhone}
+                                            onChange={(v) => handleChange(v.currentTarget.value, "contactPhone")} />
+                                    </Form.Field>
+                                    <Form.Field >
+                                        <label>Email de contacto</label>
+                                        <input placeholder=''
+                                            value={formData.contactEmail}
+                                            onChange={(v) => handleChange(v.currentTarget.value, "contactEmail")} />
+                                    </Form.Field>
+                                    <Form.Field>
+                                        <label>Domicilio</label>
+                                        <input placeholder=''
+                                            value={formData.address}
+                                            onChange={(v) => handleChange(v.currentTarget.value, "address")} />
+                                    </Form.Field>
+                                    <Form.Field>
+                                        <label>Comentario</label>
+                                        <textarea placeholder=''
+                                            value={formData.comment}
+                                            onChange={(v) => handleChange(v.currentTarget.value, "comment")} />
+                                    </Form.Field>
+                                    <button type="submit" hidden={true} />
+                                </Form>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Segment placeholder>
+                                    <Header icon>
+                                        <Icon name="file image outline" />
+                                        Imagen de perfil
+                                    </Header>
+                                    <Button primary onClick={() => {
+                                        var ref = fileRef.current
+                                        if (ref !== null) {
+                                            ref.click()
+                                        }
+                                    }}>Subir Imagen</Button>
+                                    <input ref={fileRef} type="file" hidden />
+                                </Segment>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Segment>
             </Modal.Content>
             <Modal.Actions>
                 <Button color='black' onClick={onClose}>
