3f8b4b98a7cb4328b39fc1edf832788db2adc1e7
Federico Jure <fedejure@gmail.com>
2021-09-06 22:45:00 -0300

    add days

---
+++ src/components/createProductModal/Weekdays.tsx
@@ -0,0 +1,9 @@
+export enum Weekdays {
+    Monday = "Lunes",
+    Tuesday = "Martes",
+    Wednesday = "Miercoles",
+    Thursday = "Jueves",
+    Friday = "Viernes",
+    Saturday = "Sabado",
+    Sunday = "Domingo"
+}
