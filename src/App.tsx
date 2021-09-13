a7673cf9f4b8988c2d030456465b9d4bf6236645
federico.jure <federico.jure@etermax.com>
2021-09-13 11:35:20 -0300

    add global error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
+import { ErrorBoundary } from "react-error-boundary"
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import Home from "./screens/home/Home";
 import { Provider } from 'react-redux';
 import { store } from "./store"
+import { Button, Header, Icon, Segment } from "semantic-ui-react";
+
+const OurFallbackComponent = ({ error, componentStack, resetErrorBoundary }: any) => {
+  return (
+<Segment placeholder style={{minHeight: "100vh"}}>
+    <Header icon>
+      <Icon name="bug" />
+      Ocurrio un error inesperado! Vuelva a intentar
+    </Header>
+    <Button primary onClick={resetErrorBoundary}>Recargar</Button>
+  </Segment>
+  );
+};
+
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <Home />
-      </div>
-    </Provider>
+    <ErrorBoundary FallbackComponent={OurFallbackComponent}>
+      <Provider store={store}>
+        <div className="App">
+          <Home />
+        </div>
+      </Provider>
+    </ErrorBoundary>
+
   );
 }
 
