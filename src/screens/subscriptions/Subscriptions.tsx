7ee4ad303ef908570e4a6bef23cbc88ce6ba3cdc
Federico Jure <fedejure@gmail.com>
2021-09-14 23:44:44 -0300

    add multiple lists on subcscription screen (mocked)

--- src/screens/subscriptions/Subscriptions.tsx
+++ src/screens/subscriptions/Subscriptions.tsx
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux'
 import { useEffect, useState } from 'react'
 import { Dispatch } from 'redux'
-import { Button, List, Header, Divider } from 'semantic-ui-react'
+import { Button, List, Header, Grid } from 'semantic-ui-react'
 import { StoreState } from '../../store'
 import { CreateSubscriptionModal } from "../../components/createSubscriptionModal/CreateSubscriptionModal"
 import { SubscriptionPayload } from '../../modules/subscription/SubscriptionPayload'
@@ -34,56 +34,78 @@ const Subscriptions = ({ subscriptions, createSubscription, fetchSubscriptions }
         if (page > 0)
             fetchSubscriptions({
                 page,
-                append: true
+                append: true,
+                filterByContent: filter
             })
     }, [page])
 
     useEffect(() => {
         fetchSubscriptions({
-            page,
+            page: 0,
             append: false,
             filterByContent: filter
         })
+        setPage(0)
     }, [filter])
+
+    const SubscriptionSection = <>
+        <Grid.Row columns={2}>
+            <Grid.Column><h3>Subscriptions</h3></Grid.Column>
+            <Grid.Column><Button color="blue" circular icon="plus" onClick={() => setCreationModalOpen(true)} /></Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns={1}>
+            <FilterInput
+                tagOptions={[
+                    {
+                        key: PayState.DEBT,
+                        text: PayState.DEBT,
+                        value: PayState.DEBT,
+                        label: { color: 'red', empty: true, circular: true }
+                    },
+                    {
+                        key: PayState.ON_DAY,
+                        text: PayState.ON_DAY,
+                        value: PayState.ON_DAY,
+                        label: { color: 'green', empty: true, circular: true }
+                    }
+                ]}
+                onUserTypeFilterChange={(f: string[]) => { }}
+                onCustomChange={(f: string[]) => {
+                    setPage(0)
+                    setFilter(fi => f)
+                }}
+            />
+        </Grid.Row>
+        <List divided verticalAlign="middle" style={{ height: "30vh", width: "100%", overflowY: "scroll" }}>
+            <InfiniteScroll
+                as={List.Item}
+                onLoadMore={() => setPage(page => page + 1)}
+                data={subscriptions.map(s => <SubscriptionCard subscription={s} />)} />
+        </List></>
 
 
     return (<div>
         {creationModalOpen && <CreateSubscriptionModal onClose={() => setCreationModalOpen(false)}
             onSubmit={handleSubmit} />}
-        <Button color="blue" circular icon="plus" onClick={() => setCreationModalOpen(true)} />
+
         <Header as='h2' floated='left'>
             Suscripciones
         </Header>
 
-        <FilterInput
-            tagOptions={[
-                {
-                    key: PayState.DEBT,
-                    text: PayState.DEBT,
-                    value: PayState.DEBT,
-                    label: { color: 'red', empty: true, circular: true }
-                },
-                {
-                    key: PayState.ON_DAY,
-                    text: PayState.ON_DAY,
-                    value: PayState.ON_DAY,
-                    label: { color: 'green', empty: true, circular: true }
-                }
-            ]}
-            onUserTypeFilterChange={(f: string[]) => { }}
-            onCustomChange={(f: string[]) => {
-                setPage(0)
-                setFilter(fi => f)
-            }}
-        />
+        <Grid divided='vertically' style={{ width: "100%" }}>
+            <Grid.Row columns={1}>
+                {SubscriptionSection}
+            </Grid.Row>
 
-        <Divider />
-        <List divided verticalAlign="middle">
-            <InfiniteScroll
-                as={List.Item}
-                onLoadMore={() => setPage(page => page + 1)}
-                data={subscriptions.map(s => <SubscriptionCard subscription={s} />)} />
-        </List>
+            <Grid.Row columns={2}>
+                <Grid.Column>
+                    {SubscriptionSection}
+                </Grid.Column>
+                <Grid.Column>
+                    {SubscriptionSection}
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
     </div>)
 }
 
