837a6d81b56b2808b260865a9641631b5f51a202
Federico Jure <fedejure@gmail.com>
2021-09-15 16:01:13 -0300

    add validations on forms

--- src/components/createUserModal/CreateUserModal.tsx
+++ src/components/createUserModal/CreateUserModal.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useEffect, useRef, useState } from "react"
-import { Button, Modal, Form, Grid, Divider, Segment, Icon, Header, Image, Dropdown } from 'semantic-ui-react'
+import { Button, Modal, Form, Grid, Divider, Segment, Icon, Header, Image, Dropdown, TextArea } from 'semantic-ui-react'
 import { User } from "../../modules/users/User"
 import { UserPayload } from "../../modules/users/UserPayload"
 import { UserType } from "../../modules/users/UserType"
@@ -11,6 +11,7 @@ export const CreateUserModal = ({ onClose, onSubmit, initialData }:
     { onClose: any, onSubmit: any, initialData?: User | null }) => {
     const allowedTypes = ["image/png", "image/jpeg"]
     const fileRef = useRef<HTMLInputElement>(null)
+    const [submitted, setSubmitted] = useState(false)
     const [brothers, setBrothers] = useState<User[]>([])
     const [users, setUsers] = useState<User[]>([])
     const [formData, setFormData] = useState<UserPayload>(
@@ -31,12 +32,14 @@ export const CreateUserModal = ({ onClose, onSubmit, initialData }:
                 dni: ""
             })
     const handleSubmit = () => {
+        setSubmitted(true)
         if (formData.name !== "" &&
             formData.lastname !== "" &&
             formData.address !== "" &&
             formData.contactEmail !== "" &&
             formData.contactPhone !== "" &&
-            formData.birthDate !== defaultDate)
+            formData.birthDate !== defaultDate &&
+            formData.dni !== "")
             onSubmit(formData)
     }
 
@@ -88,11 +91,12 @@ export const CreateUserModal = ({ onClose, onSubmit, initialData }:
             <Modal.Header>{initialData ? "Edición de persona" : "Creación de persona"}</Modal.Header>
             <Modal.Content>
                 <Segment placeholder>
-                    <Grid columns={2} stackable>
-                        <Divider vertical />
-                        <Grid.Row>
-                            <Grid.Column verticalAlign='middle'>
-                                <Form onSubmit={handleSubmit}>
+                    <Form onSubmit={handleSubmit}>
+                        <Grid columns={2} stackable>
+                            <Divider vertical />
+                            <Grid.Row>
+                                <Grid.Column verticalAlign='middle'>
+
                                     <Form.Field>
                                         <label>Tipo</label>
                                         <Dropdown fluid selection
@@ -105,44 +109,84 @@ export const CreateUserModal = ({ onClose, onSubmit, initialData }:
                                             }))}
                                             onChange={(e, data) => handleChange(data.value, "type")} />
                                     </Form.Field>
-                                    <Form.Field>
+                                    <Form.Field
+                                        error={submitted && formData.name.length == 0 ? {
+                                            content: 'Ingresar Nombre',
+                                            pointing: 'below',
+                                        } : false}
+                                    >
                                         <label>Nombre</label>
                                         <input placeholder=''
                                             value={formData.name}
                                             onChange={(v) => handleChange(v.currentTarget.value, "name")} />
                                     </Form.Field>
-                                    <Form.Field >
+                                    <Form.Field
+                                        error={submitted && formData.lastname.length == 0 ? {
+                                            content: 'Ingresar Apellido',
+                                            pointing: 'below',
+                                        } : false}
+                                    >
                                         <label>Apellido</label>
                                         <input placeholder=''
                                             value={formData.lastname}
                                             onChange={(v) => handleChange(v.currentTarget.value, "lastname")} />
                                     </Form.Field>
-                                    <Form.Field >
+                                    <Form.Field
+                                        error={submitted && formData.dni.length == 0 ? {
+                                            content: 'Ingresar Documento',
+                                            pointing: 'below',
+                                        } : false}
+                                    >
                                         <label>Documento (DNI)</label>
                                         <input placeholder=''
                                             value={formData.dni}
                                             onChange={(v) => handleChange(v.currentTarget.value, "dni")} />
                                     </Form.Field>
-                                    <Form.Field >
+                                    <Form.Field
+                                        error={submitted && formData.birthDate == defaultDate ? {
+                                            content: 'Ingresar Fecha de nacimiento',
+                                            pointing: 'below',
+                                        } : false}
+                                    >
                                         <label>Fecha de nacimiento</label>
                                         <input placeholder=''
                                             type="date"
                                             value={formData.birthDate && formData.birthDate !== defaultDate ? formData.birthDate.toISOString().split("T")[0] : ""}
                                             onChange={(v) => handleChange(new Date(v.currentTarget.value), "birthDate")} />
                                     </Form.Field>
-                                    <Form.Field >
+                                    <Form.Field
+                                        error={submitted && formData.contactPhone.length == 0 ? {
+                                            content: 'Ingresar Telefono',
+                                            pointing: 'below',
+                                        } : false}
+                                    >
                                         <label>Teléfono de contacto</label>
                                         <input placeholder=''
                                             value={formData.contactPhone}
                                             onChange={(v) => handleChange(v.currentTarget.value, "contactPhone")} />
                                     </Form.Field>
-                                    <Form.Field >
+                                    <Form.Field
+                                        error={submitted && formData.contactEmail.length == 0 ? {
+                                            content: 'Ingresar Email',
+                                            pointing: 'below',
+                                        } : false}
+                                    >
                                         <label>Email de contacto</label>
                                         <input placeholder=''
                                             value={formData.contactEmail}
                                             onChange={(v) => handleChange(v.currentTarget.value, "contactEmail")} />
                                     </Form.Field>
-                                    <Form.Field>
+
+                                    <input type="submit" hidden={true} />
+
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Form.Field
+                                        error={submitted && formData.address.length == 0 ? {
+                                            content: 'Ingresar Direccion',
+                                            pointing: 'below',
+                                        } : false}
+                                    >
                                         <label>Domicilio</label>
                                         <input placeholder=''
                                             value={formData.address}
@@ -150,48 +194,44 @@ export const CreateUserModal = ({ onClose, onSubmit, initialData }:
                                     </Form.Field>
                                     <Form.Field>
                                         <label>Comentario</label>
-                                        <textarea placeholder=''
+                                        <TextArea placeholder=''
                                             value={formData.comment}
                                             onChange={(v) => handleChange(v.currentTarget.value, "comment")} />
                                     </Form.Field>
-                                    <button type="submit" hidden={true} />
-                                </Form>
-                            </Grid.Column>
-                            <Grid.Column>
-                                <Segment placeholder>
-                                    {formData.profilePicture ? <Image fluid src={formData.profilePicture} /> :
-                                        <Header icon>
-                                            <Icon name="file image outline" />
-                                            Imagen de perfil
-                                        </Header>}
-                                    <Divider />
-                                    <Button primary onClick={() => {
-                                        var ref = fileRef.current
-                                        if (ref !== null) {
-                                            ref.click()
-                                        }
-                                    }}>Subir Imagen</Button>
-                                    <input ref={fileRef} type="file" hidden onChange={handleImageChange} />
-                                </Segment>
-                                <Form.Field>
-                                    <label>Hermanos</label>
-                                    <Dropdown fluid selection
-                                        search
-                                        onSearchChange={(_, value) => handleBrotherSearch(value.searchQuery)}
-                                        multiple
-                                        value={formData.brothers || []}
-                                        onChange={(e, data) => handleChange(data.value, "brothers")}
-                                        options={[...brothers, ...users]
-                                            .filter(user => user.id !== initialData?.id)
-                                            .map(p => ({
-                                            key: p.name,
-                                            text: `${p.lastname}, ${p.name}, ${p.dni}`,
-                                            value: p.id
-                                        }))} />
-                                </Form.Field>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
+                                    <Form.Field>
+                                        <label>Hermanos</label>
+                                        <Dropdown fluid selection
+                                            search
+                                            onSearchChange={(_, value) => handleBrotherSearch(value.searchQuery)}
+                                            multiple
+                                            value={formData.brothers || []}
+                                            onChange={(e, data) => handleChange(data.value, "brothers")}
+                                            options={[...brothers, ...users]
+                                                .filter(user => user.id !== initialData?.id)
+                                                .map(p => ({
+                                                    key: p.name,
+                                                    text: `${p.lastname}, ${p.name}, ${p.dni}`,
+                                                    value: p.id
+                                                }))} />
+                                    </Form.Field><Segment placeholder>
+                                        {formData.profilePicture ? <Image fluid src={formData.profilePicture} /> :
+                                            <Header icon>
+                                                <Icon name="file image outline" />
+                                                Imagen de perfil
+                                            </Header>}
+                                        <Divider />
+                                        <Button primary onClick={() => {
+                                            var ref = fileRef.current
+                                            if (ref !== null) {
+                                                ref.click()
+                                            }
+                                        }}>Subir Imagen</Button>
+                                        <input ref={fileRef} type="file" hidden onChange={handleImageChange} />
+                                    </Segment>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Form>
                 </Segment>
             </Modal.Content>
             <Modal.Actions>
