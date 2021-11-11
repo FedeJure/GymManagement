d9b209147fd9f68e25e77755cdbde8f73e588937
Federico Jure <fjure@atixlabs.com>
2021-11-11 00:19:25 -0300

    add label to canceled orders

--- src/components/orderCard/OrderCard.tsx
+++ src/components/orderCard/OrderCard.tsx
@@ -1,11 +1,12 @@
-import { Button, Icon, List } from "semantic-ui-react"
+import { Button, Icon, Label, List } from "semantic-ui-react"
 import { Order } from "../../modules/order/Order"
 
 export const OrderCard = ({ order, handleCancel }: { order: Order, handleCancel: Function }) => (
     <List.Item style={{ padding: "0.5em", width: "100%" }}>
-        {order.amountPayed <= 0 && <List.Content floated='right'>
+        {order.amountPayed <= 0 && !order.cancelled && !order.completed && <List.Content floated='right'>
             <Button icon compact onClick={() => handleCancel()}><Icon name="trash"></Icon>Cancelar orden</Button>
         </List.Content>}
+        {order.cancelled && <List.Content floated='right'><Label>Cancelado</Label></List.Content>}
 
         <List.Content floated="left">Adeuda pagos</List.Content>
         <List.Content floated="left">{order.productName}</List.Content>
