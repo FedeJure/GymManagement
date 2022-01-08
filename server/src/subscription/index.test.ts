1abb767c7dd984c77be281f6cec55ca28ed1085c
Federico Jure <fedejure@gmail.com>
2022-01-08 13:55:17 -0300

    add save ne subscription test

--- server/src/subscription/index.test.ts
+++ server/src/subscription/index.test.ts
@@ -1,25 +1,31 @@
 import { MockDb } from "../test/db";
-import { getSubscriptionModel } from "../mongoClient";
+import { getSubscriptions, saveSubscription } from ".";
+import { InitProductOnDb, InitUserOnDb, MockSubscriptionPayload } from "../test/mocks";
 
 const db = new MockDb();
 beforeAll(async () => await db.connect());
-afterEach(async () => {
-  await db.clearDatabase();
+beforeEach(async () => {
+  await InitUserOnDb();
+  await InitProductOnDb();
 });
+afterEach(async () => await db.clearDatabase());
 afterAll(async () => await db.closeDatabase());
 
 describe("Subscriptions", () => {
   it("get subscriptions on empty db", (done) => {
-    try {
-      const model = getSubscriptionModel();
-      model.find({}).then(subscriptions => {
-        expect(subscriptions.length).toBe(0);
-        done()
-      })
-      
-    } catch (error) {
-      done(error)
-    }
+    getSubscriptions({ page: 0, step: 10 }).then((subscriptions) => {
+      expect(subscriptions.length).toBe(0);
+      done();
+    });
+  });
 
+  it("save new subscription", (done) => {
+    saveSubscription(MockSubscriptionPayload).then((subscription) => {
+      expect(subscription?.user.id).toEqual(MockSubscriptionPayload.userId)
+      expect(subscription?.product.id).toEqual(MockSubscriptionPayload.productId)
+      expect(subscription?.initialTime.toISOString()).toEqual(MockSubscriptionPayload.initialTime.toISOString())
+      expect(subscription?.endTime?.toISOString()).toEqual(MockSubscriptionPayload.endTime?.toISOString())
+      done();
+    });
   });
 });
