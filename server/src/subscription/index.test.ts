218d0c39558184e4766077bc16ad8c3dd83e4617
Federico Jure <fedejure@gmail.com>
2022-01-08 16:22:46 -0300

    test discounts

--- server/src/subscription/index.test.ts
+++ server/src/subscription/index.test.ts
@@ -1,6 +1,7 @@
 import { MockDb } from "../test/db";
 import { getSubscriptions, saveSubscription } from ".";
 import {
+  InitBrothersOnDb,
   InitProductOnDb,
   InitUserOnDb,
   MockSubscriptionPayload,
@@ -63,10 +64,136 @@ describe("Subscriptions", () => {
     saveSubscription(mockSubscriptionPayload).then((subscription) => {
       getOrders({ page: 0, step: 10 }).then((orders) => {
         expect(orders.length).toBe(1);
-        expect(orders[0].subscriptionId.toString()).toEqual(subscription?.id.toString())
-        expect(orders[0].productId.toString()).toEqual(subscription?.product.id.toString())
-        expect(orders[0].userId.toString()).toEqual(subscription?.user.id.toString())
+        expect(orders[0].subscriptionId.toString()).toEqual(
+          subscription?.id.toString()
+        );
+        expect(orders[0].productId.toString()).toEqual(
+          subscription?.product.id.toString()
+        );
+        expect(orders[0].userId.toString()).toEqual(
+          subscription?.user.id.toString()
+        );
         done();
+      });
+    });
+  });
+
+  it("create order without discount", (done) => {
+    const mockSubscriptionPayload: SubscriptionPayload = {
+      ...MockSubscriptionPayload,
+      initialTime: new Date(Date.now()),
+    };
+    saveSubscription(mockSubscriptionPayload).then((subscription) => {
+      getOrders({ page: 0, step: 10 }).then((orders) => {
+        expect(orders[0].amount).toEqual(subscription?.product.price);
+        done();
+      });
+    });
+  });
+
+  it("create order with special discount", (done) => {
+    const mockSubscriptionPayload: SubscriptionPayload = {
+      ...MockSubscriptionPayload,
+      initialTime: new Date(Date.now()),
+      specialDiscount: 5, // percent
+    };
+    saveSubscription(mockSubscriptionPayload).then((subscription) => {
+      getOrders({ page: 0, step: 10 }).then((orders) => {
+        expect(orders[0].amount).toEqual(
+          (subscription?.product.price || 0) * 0.95
+        );
+        done();
+      });
+    });
+  });
+
+  it("create order with one brother", (done) => {
+    InitBrothersOnDb(1).then(() => {
+      const mockSubscriptionPayload: SubscriptionPayload = {
+        ...MockSubscriptionPayload,
+        initialTime: new Date(Date.now()),
+      };
+      saveSubscription(mockSubscriptionPayload).then((subscription) => {
+        console.log(subscription)
+        getOrders({ page: 0, step: 10 }).then((orders) => {
+          expect(orders[0].amount).toEqual(
+            (subscription?.product.price || 0) * 0.95
+          );
+          done();
+        });
+      });
+    });
+  });
+
+  it("create order with two brothers", (done) => {
+    InitBrothersOnDb(2).then(() => {
+      const mockSubscriptionPayload: SubscriptionPayload = {
+        ...MockSubscriptionPayload,
+        initialTime: new Date(Date.now()),
+      };
+      saveSubscription(mockSubscriptionPayload).then((subscription) => {
+        console.log(subscription)
+        getOrders({ page: 0, step: 10 }).then((orders) => {
+          expect(orders[0].amount).toEqual(
+            (subscription?.product.price || 0) * 0.90
+          );
+          done();
+        });
+      });
+    });
+  });
+
+  it("create order with three brothers", (done) => {
+    InitBrothersOnDb(3).then(() => {
+      const mockSubscriptionPayload: SubscriptionPayload = {
+        ...MockSubscriptionPayload,
+        initialTime: new Date(Date.now()),
+      };
+      saveSubscription(mockSubscriptionPayload).then((subscription) => {
+        console.log(subscription)
+        getOrders({ page: 0, step: 10 }).then((orders) => {
+          expect(orders[0].amount).toEqual(
+            (subscription?.product.price || 0) * 0.85
+          );
+          done();
+        });
+      });
+    });
+  });
+
+  it("create order with four brothers", (done) => {
+    InitBrothersOnDb(4).then(() => {
+      const mockSubscriptionPayload: SubscriptionPayload = {
+        ...MockSubscriptionPayload,
+        initialTime: new Date(Date.now()),
+      };
+      saveSubscription(mockSubscriptionPayload).then((subscription) => {
+        console.log(subscription)
+        getOrders({ page: 0, step: 10 }).then((orders) => {
+          expect(orders[0].amount).toEqual(
+            (subscription?.product.price || 0) * 0.8
+          );
+          done();
+        });
+      });
+    });
+  });
+
+  it("create order with max discount applied", (done) => {
+    InitBrothersOnDb(4).then(() => {
+      const mockSubscriptionPayload: SubscriptionPayload = {
+        ...MockSubscriptionPayload,
+        initialTime: new Date(Date.now()),
+        specialDiscount: 30
+      };
+      saveSubscription(mockSubscriptionPayload).then((subscription) => {
+        console.log(subscription)
+        getOrders({ page: 0, step: 10 }).then((orders) => {
+          expect(orders[0].amount).toEqual(
+            (subscription?.product.price || 0) * 0.7
+          );
+          done();
+        });
       });
     });
   });
--- server/src/test/mocks.ts
+++ server/src/test/mocks.ts
@@ -7,49 +7,87 @@ import { UserPayload } from "../../../src/modules/users/UserPayload";
 import { UserType } from "../../../src/modules/users/UserType";
 import { getProductModel, getUserModel } from "../mongoClient";
 
-export const MockUserId = new ObjectId()
-export const MockProductId = new ObjectId()
+export const MockUserId = new ObjectId();
+export const MockBrotherIds = [
+  new ObjectId(),
+  new ObjectId(),
+  new ObjectId(),
+  new ObjectId(),
+  new ObjectId(),
+];
+export const MockProductId = new ObjectId();
 
 export const InitUserOnDb = async () => {
-    const userModel = getUserModel()
-    return await userModel.create({...MockUserPayload, _id: MockUserId})
-}
+  const userModel = getUserModel();
+  return await userModel.create({ ...MockUserPayload, _id: MockUserId });
+};
 
 export const InitProductOnDb = async () => {
-    const productModel = getProductModel()
-    return await productModel.create({...MockProductPayload, _id: MockProductId})
-}
+  const productModel = getProductModel();
+  return await productModel.create({
+    ...MockProductPayload,
+    _id: MockProductId,
+  });
+};
+
+export const InitBrothersOnDb = async (brothersCount: 1 | 2 | 3 | 4 | 5) => {
+  const userModel = getUserModel();
+
+  for (let i = 0; i < brothersCount; i++) {
+    const brotherPayload = {
+      type: UserType.STUDENT,
+      name: "Brother " + i,
+      lastname: "Mock Lastname",
+      contactEmail: "mock@email.com",
+      contactPhone: "99999999",
+      address: "Fake address 123",
+      birthDate: new Date(),
+      comment: "Fake comment",
+      familiars: [MockUserId],
+      productsSubscribed: [],
+      profilePicture: "",
+      dni: "112333232",
+    };
+    const brother = await userModel.create({...brotherPayload, _id: MockBrotherIds[i]});
+    const user = await userModel.findById(MockUserId);
+    if (!user) return;
+    await userModel.updateOne({
+      _id: MockUserId,
+      familiars: [...user.familiars, brother._id],
+    });
+  }
+};
 
 export const MockUserPayload: UserPayload = {
-    type: UserType.STUDENT,
-    name: "Mock User",
-    lastname: "Mock Lastname",
-    contactEmail: "mock@email.com",
-    contactPhone: "99999999",
-    address: "Fake address 123",
-    birthDate: new Date(),
-    comment: "Fake comment",
-    familiars: [],
-    productsSubscribed: [],
-    profilePicture: "",
-    dni: "112333232"
-}
+  type: UserType.STUDENT,
+  name: "Mock User",
+  lastname: "Mock Lastname",
+  contactEmail: "mock@email.com",
+  contactPhone: "99999999",
+  address: "Fake address 123",
+  birthDate: new Date(),
+  comment: "Fake comment",
+  familiars: [],
+  productsSubscribed: [],
+  profilePicture: "",
+  dni: "112333232",
+};
 
 export const MockProductPayload: ProductPayload = {
-    name: "Mock Product",
-    payType: PayType.MONTHLY,
-    price: 1000,
-    twoSubscriptionsDiscount: 0,
-    threeSubscriptionsDiscount: 0,
-    fourSubscriptionsDiscount: 0,
-    fiveOrMoreSubscriptionsDiscount: 0,
-    daysInWeek: [Weekdays.Monday]
-}
+  name: "Mock Product",
+  payType: PayType.MONTHLY,
+  price: 1000,
+  twoSubscriptionsDiscount: 5,
+  threeSubscriptionsDiscount: 10,
+  fourSubscriptionsDiscount: 15,
+  fiveOrMoreSubscriptionsDiscount: 20,
+  daysInWeek: [Weekdays.Monday],
+};
 
 export const MockSubscriptionPayload: SubscriptionPayload = {
-    userId: MockUserId.toString(),
-    productId: MockProductId.toString(),
-    specialDiscount: 0,
-    initialTime: new Date(),
-    comment: "comment"
-}
+  userId: MockUserId.toString(),
+  productId: MockProductId.toString(),
+  specialDiscount: 0,
+  initialTime: new Date(),
+  comment: "comment",
+};
