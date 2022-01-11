5e420341e5b1f64acd3ec0199a90aaafe178d2d7
Federico Jure <fedejure@gmail.com>
2022-01-11 16:46:51 -0300

    fix get order method

--- src/components/orderCard/OrderCard.tsx
+++ src/components/orderCard/OrderCard.tsx
@@ -1,22 +1,56 @@
-import { Button, Card, Icon, Label, List } from "semantic-ui-react"
-import { Order } from "../../modules/order/Order"
+import { Button, Card, Icon, Label, List } from "semantic-ui-react";
+import { Order } from "../../modules/order/Order";
+import { getMonth } from "../../utils/date";
 
-export const OrderCard = ({ order, handleCancel, handleGenerate }: { order: Order, handleCancel: Function, handleGenerate: Function }) => (
-    <List.Item style={{ padding: "0.5em", width: "100%" }}>
-        <Card fluid color="teal">
-            <Card.Content>
-                {order.amountPayed <= 0 && !order.cancelled && !order.completed && <List.Content floated='right'>
-                    <Button icon compact primary onClick={() => handleGenerate()}><Icon name="payment"></Icon> Generar pago</Button>
-                    <Button icon compact onClick={() => handleCancel()}><Icon name="trash"></Icon>Cancelar orden</Button>
-                </List.Content>}
-                {order.cancelled && <List.Content floated='right'><Label>Cancelado</Label></List.Content>}
-                <List.Content floated="left">{order.productName}</List.Content>
-                <List.Content floated="left">{order.userName}</List.Content>
+export const OrderCard = ({
+  order,
+  handleCancel,
+  handleGenerate,
+}: {
+  order: Order;
+  handleCancel: Function;
+  handleGenerate: Function;
+}) => (
+  <List.Item style={{ padding: "0.5em", width: "100%" }}>
+    <Card fluid color="teal">
+      <Card.Content>
+        {order.amountPayed <= 0 && !order.cancelled && !order.completed && (
+          <List.Content floated="right">
+            <Button icon compact primary onClick={() => handleGenerate()}>
+              <Icon name="payment"></Icon> Generar pago
+            </Button>
+            <Button icon compact onClick={() => handleCancel()}>
+              <Icon name="trash"></Icon>Cancelar orden
+            </Button>
+          </List.Content>
+        )}
+        {order.cancelled && (
+          <List.Content floated="right">
+            <Label>Cancelado</Label>
+          </List.Content>
+        )}
+        <List.Content floated="left">
+          <Label horizontal>
+            {getMonth(order.periodPayed.getMonth())}
+            <br />
+            {order.periodPayed.getFullYear()}
+          </Label>
+          {order.productName}
+        </List.Content>
+        <List.Content floated="left">{order.userName}</List.Content>
 
-                <List.Content floated="left">Pagado <b>$ARS {order.amountPayed}</b> de <b>$ARS{order.amount}</b></List.Content>
-                {order.totalDiscount > 0 && <List.Content floated="left">Descuento aplicado: {order.totalDiscount}%</List.Content>}
-                <List.Content floated="left">Fecha de creacion {order.emittedDate.toLocaleDateString()}</List.Content>
-            </Card.Content>
-        </Card>
-    </List.Item>
-)
+        <List.Content floated="left">
+          Pagado <b>$ARS {order.amountPayed}</b> de <b>$ARS{order.amount}</b>
+        </List.Content>
+        {order.totalDiscount > 0 && (
+          <List.Content floated="left">
+            Descuento aplicado: {order.totalDiscount}%
+          </List.Content>
+        )}
+        <List.Content floated="left">
+          Fecha de creacion {order.emittedDate.toLocaleDateString()}
+        </List.Content>
+      </Card.Content>
+    </Card>
+  </List.Item>
+);
---
+++ src/utils/date.ts
@@ -0,0 +1,17 @@
+const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+export function getMonth(month: number) {
+  return months[month];
+}
