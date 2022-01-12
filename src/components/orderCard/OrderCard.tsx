3bcb0c6855693966084476d13359b91b3ea9063b
Federico Jure <fedejure@gmail.com>
2022-01-11 23:38:01 -0300

    add alert

--- src/components/orderCard/OrderCard.tsx
+++ src/components/orderCard/OrderCard.tsx
@@ -14,16 +14,20 @@ export const OrderCard = ({
   <List.Item style={{ padding: "0.5em", width: "100%" }}>
     <Card fluid color="teal">
       <Card.Content>
-        {order.amountPayed < order.amount && !order.cancelled && !order.completed && (
-          <List.Content floated="right">
-            <Button icon compact primary onClick={() => handleGenerate()}>
-              <Icon name="payment"></Icon> Generar pago
-            </Button>
-            <Button icon compact onClick={() => handleCancel()}>
-              <Icon name="trash"></Icon>Cancelar orden
-            </Button>
-          </List.Content>
-        )}
+        {order.amountPayed < order.amount &&
+          !order.cancelled &&
+          !order.completed && (
+            <List.Content floated="right">
+              <Button icon compact primary onClick={() => handleGenerate()}>
+                <Icon name="payment"></Icon> Generar pago
+              </Button>
+            </List.Content>
+          )}
+        <List.Content floated="right">
+          <Button icon compact onClick={() => handleCancel()}>
+            <Icon name="trash"></Icon>Cancelar orden
+          </Button>
+        </List.Content>
         {order.cancelled && (
           <List.Content floated="right">
             <Label>Cancelado</Label>
